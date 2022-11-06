2dae84be9fbc89b19521c3d580cbac30d6c61361
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-11-06 09:20:40 -0500

    Initial Commit

---
+++ Thesis2/Thesis2App.swift
@@ -0,0 +1,17 @@
+//
+//  Thesis2App.swift
+//  Thesis2
+//
+//  Created by Tim Yoon on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Thesis2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
